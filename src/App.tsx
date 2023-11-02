93928f36e5b13492b1b14efc4d695812b81d79ac
cueltschey <chaseuelt@gmail.com>
2023-11-02 13:44:37 -0500

    fixed dictionary error

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,39 @@ import './App.css'
 import key from '../key.json'
 
 const App = () => {
-  //  get the word from the page with use effect
-  const [highlightedText, setHighlightedText] = useState('testing');
+  const [highlightedText, setHighlightedText] = useState('humiliating');
+
+
+  const apiUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${highlightedText}?key=${key.keys[0]}`;
+  const [isValid, setIsValid] = useState(true)
+  const [pronounciation, setPronounciation] = useState("")
+  let definitions: string[] = [] 
+  let usages: string[] = []
+
+  function getData(){
+    console.log("fetching")
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (Array.isArray(data) && data.length > 0) {
+          setPronounciation(data[0].hwi.prs[0].mw);
+          const results = data[0].shortdef;
+           for(var definitionObject in results){
+            definitions.push(definitionObject);
+            }
+          for(var usageObject in results){
+            usages.push(usageObject);
+            }
+          if(usages.length < 1 || definitions.length < 1) setIsValid(false)
+        } else {
+          setIsValid(false)
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching pronunciation:', error);
+        setIsValid(false)
+      });
+  }
   useEffect(() => {
     const getHighlightedText = (tab: any) => {
       chrome.scripting.executeScript({
@@ -13,7 +44,9 @@ const App = () => {
           if(window.getSelection()){
             return window.getSelection()?.toString().split(" ")[0]
           }
-          else return ""
+          else{
+            return ""
+          }
         },
       },
       (result) => {
@@ -22,6 +55,7 @@ const App = () => {
         } else {
           let text : string | undefined = result[0].result
           if(text) setHighlightedText(text);
+          getData()
         }
       });
     };
@@ -37,53 +71,30 @@ const App = () => {
         console.error("Unable to retrieve the current tab.");
       }
     });
-  }catch(error){alert("must have highlighted text")}}, []); 
+  }catch(error){console.error("must have highlighted text"), setIsValid(false)}}, []); 
 
 
-
-  // use api to get data from dictionary
-  const apiUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${highlightedText}?key=${key.keys[0]}`;
-  const [isValid, setIsValid] = useState(true)
-  const [pronounciation, setPronounciation] = useState("")
-  let definitions: string[] = [] 
-  let usages: string[] = []
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      if (Array.isArray(data) && data.length > 0) {
-        setPronounciation(data[0].hwi.prs[0].mw);
-        const results = data[0].shortdef;
-         for(var definitionObject in results){
-          definitions.push(definitionObject);
-          }
-        for(var usageObject in results){
-          usages.push(usageObject);
-          }
-      } else {
-        setIsValid(false)
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching pronunciation:', error);
-    });
-
+  //testing
+  getData()
+  console.log(definitions)
+  console.log(usages)
   return (
   <div className="main">
-      {isValid? <div></div> : <div className='wordwrap'>Error</div>}
+      {isValid? <div></div> : <div className='errormsg'>Error: word not found</div>}
       <h2>Word Chad</h2>
       <div className='wordwrap'>
         <div>{highlightedText === ""? "no highlighted text" : highlightedText}</div>
         <div>{pronounciation === ""? "invalid" : pronounciation}</div>
       </div>
-      <div>
-        {definitions.map((definition: string) => 
+      <div className='list'>
+        {definitions.map((definition: string) => ( 
           <li>{definition}</li>
-        )}
+        ))}
       </div> 
-      <div>
-        {usages.map((usage: string) => 
+      <div className='list'>
+        {usages.map((usage: string) => (
           <li>{usage}</li>
-        )}
+        ))}
       </div>
     </div>
   )
